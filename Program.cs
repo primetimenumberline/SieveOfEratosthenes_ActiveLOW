49aa53c45455bdd22f9a8cc165bf6cdff618ebf7
primetimenumberline <primetimenumberline@gmail.com>
2022-02-08 07:33:25 -0400

    Never give up

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ for(long i = 2; i*i <= n; i++)
 {  
     if ((!b[i]))
     {
-        for(long j = 2*i; j < n; j += i)
+        for(long j = 2*i; j <= n; j += i)
         {
             b[j] = true; 
         }
